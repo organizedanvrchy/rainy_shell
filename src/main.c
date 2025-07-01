b1946b7f17e489d361be439e6e199531ac7cbaa7
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-07-01 16:16:14 -0400

    Add shell built-ins and execute

--- src/main.c
+++ src/main.c
@@ -1,5 +1,58 @@
 // main.c
 
+// Built-in shell commands - forward declarations
+int rsh_cd(char **args);
+int rsh_help(char **args);
+int rsh_exit(char **args);
+
+// Built-in shell commands and corresponding functions
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &rsh_cd,
+  &rsh_help,
+  &rsh_exit,
+};
+
+int rsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+// Built-in function implementations
+int rsh_cd(char **args) {
+  if(args[1] == NULL) {
+    fprintf(stderr, "rsh: expected argument to \"cd\"\n");
+  } else {
+    if(chdir(args[1]) != 0) {
+      perror("rsh");
+    }
+  }
+  return 1;
+}
+
+int rsh_help(char **args) {
+  int i;
+  printf("V's Rainy Shell\n");
+  printf("Type program names and arguments, then hit 'Enter'.\n");
+  printf("Built-in commands are: \n");
+
+  for(i = 0; i < rsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for more information on other programs.\n");
+  return 1;
+}
+
+int rsh_exit(char **args) {
+  return 0;
+}
+
+
 // Shell start process
 int rsh_launch(char **args) {
   pid_t pid, wpid;
@@ -22,6 +75,23 @@ int rsh_launch(char **args) {
     } while(!WIFEXITED(status) && !WIFSIGNALED(status));
   }
   return 1;
+}
+
+// Shell execute - launches a Built-in or Process
+int rsh_execute(char **args) {
+  int i;
+
+  if(args[0] == NULL) {
+    // Empty command entered
+    return 1;
+  }
+
+  for(i = 0; i < rsh_num_builtins(); i++) {
+    if(strcmp(args[0], builtin_str[i]) == 0) {
+      return(*builtin_func[i])(args);
+    }
+  }
+  return rsh_launch(args);
 }
 
 // Reading Line with getline() function
