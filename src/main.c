722b27df60304750071f8195c92d2bb14afa8cfc
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-07-01 15:59:31 -0400

    Add shell process launch

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 // main.c
 
+// Shell start process
+int rsh_launch(char **args) {
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if(pid==0) {
+    // Child process
+    if(execvp(args[0], args) == -1) {
+      perror("rsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if(pid < 0) {
+    // Error forking
+    perror("rsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
 // Reading Line with getline() function
 char *rsh_read_line(void) {
   char *line = NULL;
